1a34015bb72c70f66714b71d19553111a9a0236d
sauravrijal10 <sauravrijal1011@gmail.com>
2024-01-22 15:28:25 +0545

    Minor changes

--- branch/models.py
+++ branch/models.py
@@ -6,11 +6,11 @@ class Branch(models.Model):
     country = models.ForeignKey(Country, on_delete=models.CASCADE)
     city = models.CharField(max_length=255,null=True)
     mobile = models.CharField(max_length=255)
-    telephone = models.CharField(max_length=255)
-    cr = models.CharField(max_length=255)
+    telephone = models.CharField(max_length=255, blank=True, null=True)
+    cr = models.CharField(max_length=255, blank=True, null=True)
     email = models.CharField(max_length=255, unique=True)
-    website = models.CharField(max_length=255)
-    logo = models.CharField(max_length=255)
+    website = models.CharField(max_length=255, blank=True, null=True)
+    logo = models.CharField(max_length=255, blank=True, null=True)
 
     def __str__(self):
         return self.name
