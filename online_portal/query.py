429f4e7b7a3df2685c6193c7a41e63d52473de20
sauravrijal10 <sauravrijal1011@gmail.com>
2024-03-17 00:06:21 +0545

    Added branch specific details

--- online_portal/query.py
+++ online_portal/query.py
@@ -7,16 +7,34 @@ from django.core.serializers import serialize
 from django.db.models import Count
 from django.db.models.functions import ExtractMonth
 
+from django.contrib.auth.decorators import login_required
 
+from django.db.models import F
+
+
+@login_required
 def query_view(request):
-    query1 = Payment.objects.filter(payment_status='COMPLETED').values('id', 'amount', 'payment_status', 'invoice_id')
-    count1 = query1.count()
-    query2 = Payment.objects.filter(payment_status='INCOMPLETE').values('id', 'amount', 'payment_status', 'invoice_id')
-    count2 = query2.count()
-    query3 = Customer.objects.filter(file=None).values('id', 'name', 'passport_number', 'applied_country', 'contact', 'status', 'branch', 'invoice')
-    count3 = query3.count()
-    query4 = Customer.objects.values('status').annotate(count=Count('id'))
-    query5 = Customer.objects.annotate(month=ExtractMonth('created_at')).values('month').annotate(count=Count('id'))
+    user = request.user
+    print(user)
+    if user.is_superuser or user.is_admin:
+        query1 = Payment.objects.filter(payment_status='COMPLETED').values('id', 'amount', 'payment_status', 'invoice_id')
+        count1 = query1.count()
+        query2 = Payment.objects.filter(payment_status='INCOMPLETE').values('id', 'amount', 'payment_status', 'invoice_id')
+        count2 = query2.count()
+        query3 = Customer.objects.filter(file=None).values('id', 'name', 'passport_number', 'applied_country', 'contact', 'status', 'branch', 'invoice')
+        count3 = query3.count()
+        query4 = Customer.objects.values('status').annotate(count=Count('id'))
+        query5 = Customer.objects.filter(file__isnull=False).annotate(month=ExtractMonth('created_at')).values('month').annotate(count=Count('id'))
+    else:
+        user_branch = user.branch
+        query1 = Payment.objects.filter(payment_status='COMPLETED', payment_creator__branch=user_branch).values('id', 'amount', 'payment_status', 'invoice_id')
+        count1 = query1.count()
+        query2 = Payment.objects.filter(payment_status='INCOMPLETE', payment_creator__branch=user_branch).values('id', 'amount', 'payment_status', 'invoice_id')
+        count2 = query2.count()
+        query3 = Customer.objects.filter(file=None, branch=user_branch).values('id', 'name', 'passport_number', 'applied_country', 'contact', 'status', 'branch', 'invoice')
+        count3 = query3.count()
+        query4 = Customer.objects.filter(branch=user_branch).values('status').annotate(count=Count('id'))
+        query5 = Customer.objects.filter(file__isnull=False, branch=user_branch).annotate(month=ExtractMonth('created_at')).values('month').annotate(count=Count('id'))
 
     response_data ={
         'Payment_Completed':list(query1),
@@ -26,6 +44,30 @@ def query_view(request):
         'File_Null':list(query3),
         'File_Null_Count': count3,
         'Group_By_Status':list(query4),
-        'Group_By_Month':list(query5)
+        'Group_By_Month_file':list(query5)
+    }
+    return JsonResponse(response_data, safe=False)
+
+def group_by_branch(request):
+    query1 = Payment.objects.filter(payment_status='COMPLETED').values('payment_creator__branch').annotate(
+    total_payments=Count('id'),
+    # total_amount=Sum('amount')
+    ).values('payment_creator__branch', 'total_payments','id', 'amount', 'invoice_id')
+    query2 = Payment.objects.filter(payment_status='INCOMPLETE').values('payment_creator__branch').annotate(
+    total_payments=Count('id'),
+    # total_amount=Sum('amount')
+    ).values('payment_creator__branch', 'total_payments','id', 'amount', 'invoice_id')
+    query3 = Customer.objects.filter(file=None).annotate(customer_branch=F('branch')).values('branch').annotate(count=Count('id')).values('branch', 'count')
+
+    query4 = Customer.objects.values('status', 'branch').annotate(count=Count('id')).values('status', 'branch', 'count')
+
+    query5 = Customer.objects.filter(file__isnull=False).annotate(customer_branch=F('branch')).values('branch').annotate(count=Count('id'))
+    response_data ={
+        'Payment_Completed':list(query1),
+        'Payment_Incomplete':list(query2),
+        'File_Null':list(query3),
+        # 'File_Null_Count': count3,
+        'Group_By_Status':list(query4),
+        'Group_By_Month_file':list(query5)
     }
     return JsonResponse(response_data, safe=False)
--- online_portal/urls.py
+++ online_portal/urls.py
@@ -8,7 +8,7 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from django.conf import settings
 from django.conf.urls.static import static
 
-from .query import query_view
+from .query import query_view, group_by_branch
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -30,7 +30,9 @@ urlpatterns = [
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('api/token', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/query/', query_view, name="query")
+    path('api/query/', query_view, name="query"),
+    path('api/query_branch/', group_by_branch, name="query_branch")
+
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
